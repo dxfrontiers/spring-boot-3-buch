37143229ac20cddeec7896d22c161f993d98210c
Francois Fernandes <francois.fernandes@digitalfrontiers.de>
2024-03-08 09:44:09 +0100

    Spring Boot SPA example, initial scaffold

---
+++ 11_spa/src/main/java/de/springboot3/spa/SpringBootSpaApplication.java
@@ -0,0 +1,13 @@
+package de.springboot3.spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpaApplication.class, args);
+	}
+
+}
---
+++ 11_spa/src/test/java/de/springboot3/spa/SpringBootSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.springboot3.spa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
