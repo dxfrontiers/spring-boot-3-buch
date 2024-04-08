ad8a4c885aa6acbeafdd942d65063ac98c8e2d3a
Benedikt Jerat <b.jerat@googlemail.com>
2024-04-08 21:33:26 +0200

    Minor dependency change, add @query annotation and tests for spring data

--- 30_data/commons/src/test/java/de/springbootbuch/data/commons/repositories/PersonRepositoryTest.java
+++ 30_data/commons/src/test/java/de/springbootbuch/data/commons/repositories/PersonRepositoryTest.java
@@ -1,9 +1,12 @@
 package de.springbootbuch.data.commons.repositories;
 
+import de.springbootbuch.data.commons.model.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class PersonRepositoryTest {
@@ -16,6 +19,13 @@ class PersonRepositoryTest {
 
     @Test
     public void verifyFindByFirstName() {
+        Person person = new Person().setFirstName("John").setLastName("McKinsey");
 
+        entityManager.persistAndFlush(person);
+
+        assertThat(cut.findByFirstName("John")).satisfiesExactly(
+                first -> assertThat(first).isEqualTo(person)
+        );
+        assertThat(cut.findByFirstName("Barbara")).isEmpty();
     }
 }
